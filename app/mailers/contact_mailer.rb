c8db09a09f2560a6c8b436346d3c1d8c83b2e166
suresh nainamsetty <sureshprince237@gmail.com>
2019-01-03 08:59:04 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+    default to: 'sureshprince237@gmail.com'
+    
+    def contact_email(name, email, body)
+       @name = name
+       @email = email
+       @body = body
+       
+       mail(from: email, subject: 'contact form Message')
+    end
+end
